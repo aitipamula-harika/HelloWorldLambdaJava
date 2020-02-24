3dd4cef7da72f33b143bbd9780159f33dd04c751
Harika444 <47811739+Harika444@users.noreply.github.com>
2020-02-24 14:17:37 +0530

    Update helloworldTest.java

--- src/test/java/book/helloworldTest.java
+++ src/test/java/book/helloworldTest.java
@@ -8,6 +8,7 @@ public class HelloTest {
   @Test
   public void successfulResponse() {
     HelloWorld app = new HelloWorld();
+    String content = result.getBody();
     assertNotNull(content);
     assertTrue(content.contains("\"Hi prod\""));
   }
